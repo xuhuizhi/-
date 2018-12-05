e33650be354ec9628cd0a65dca0f0c941b620aca
xuhuizhi <798186276@qq.com>
2018-12-05 21:16:01 +0800

    客户端

---
+++ Client/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Client_Socket a = new Client_Socket();
+        a.make_connect();
+    }
+}
---
+++ Client/src/Main2.java
@@ -0,0 +1,6 @@
+public class Main2 {
+    public static void main(String[] args) {
+        Client_Socket b = new Client_Socket();
+        b.make_connect();
+    }
+}
